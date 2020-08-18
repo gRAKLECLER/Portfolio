0f623cca26ef634e69922759dac2eee6518bfd99
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-08-18 19:58:08 +0200

    finish the structure of my burger menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-let exit = document.querySelector(".close");
+let menu = document.querySelector(".menuBurger");
+let bar = document.querySelectorAll(".menuBurger__bar")
 let nav = document.querySelector("#header");
 
-exit.addEventListener('click', () => {
+console.log(bar)
+
+menu.addEventListener('click', () => {
   nav.classList.toggle('hidden')
+  for (let index = 0; index < bar.length; index++) {
+    bar[index].classList.toggle('active')
+  }
 })
